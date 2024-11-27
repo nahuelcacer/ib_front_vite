89d481ed6e3e558de430e9e1ba57f1b8aab8e297
Nahuel <nahue_cge@hotmail.com>
2024-11-27 15:56:24 -0300

    table mov dia

--- src/pages/MovimientoDia.jsx
+++ src/pages/MovimientoDia.jsx
@@ -17,14 +17,14 @@ const MovimientoDia = () => {
   }, [])
   const handleSelectBank = async (e) => {
     const bank = banks.find(bank => bank.value === e.target.value)
-    console.log(bank)
+    
     const response = await getMovimientoDia(bank)
     setMovimientos(response.movements_detail)
-    console.log(response)
+    
   }
   return (
-    <div>
-      <h1 className='text-2xl font-bold'>Movimiento del día</h1>
+    <div className='flex flex-col gap-2'>
+      <h1 className='text-2xl font-bold'>Movimientos del día</h1>
       <div className='grid grid-cols-[1fr_3fr] gap-2'>
         <Select label='Selecciona un banco' onChange={handleSelectBank} >
           {
@@ -50,14 +50,29 @@ const MovimientoDia = () => {
         removeWrapper
         >
           <TableHeader>
-            <TableColumn>perro</TableColumn>
+            <TableColumn>ID</TableColumn>
+            <TableColumn>Desc (IB)</TableColumn>
+            <TableColumn>Desc (Banco)</TableColumn>
+            <TableColumn>Cuit Cliente</TableColumn>
+            <TableColumn>Desc (depositante)</TableColumn>
+            <TableColumn>Importe</TableColumn>
+            <TableColumn>Fecha de procesamiento</TableColumn>
+            <TableColumn>Tipo</TableColumn>
+
           </TableHeader>
           <TableBody emptyContent={<Spinner></Spinner>}>
           {movimientos?.map((mov,index) => {
             // destructurar mov 
             return (
               <TableRow key={index}>
+                <TableCell>{mov.id}</TableCell>
+                <TableCell>{mov.code_description_ib}</TableCell>
+                <TableCell>{mov.code_description_bank}</TableCell>
+                <TableCell>{mov.customer_cuit || ""}</TableCell>
+                <TableCell>{mov.depositor_description || ""}</TableCell>
                 <TableCell>{mov.amount}</TableCell>
+                <TableCell>{mov.process_date}</TableCell>
+                <TableCell>{mov.debit_credit_type}</TableCell>
               </TableRow>
             )
           })}
