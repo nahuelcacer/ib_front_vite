c6bf802c232cb82a9c3f9867d4448ec85ca93c8c
Nahuel <nahue_cge@hotmail.com>
2024-12-17 15:52:28 -0300

    agrega los items u arregle el submit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { getSaldos } from "../service/service.saldos";
 import { formatArs } from "../utils/formatter";
-import { Avatar, Button, DatePicker, Input, Select, SelectItem, Table, TableBody, TableHeader, TableRow } from "@nextui-org/react";
+import { Avatar, Button, DatePicker, Input, Select, SelectItem, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import { ProfileContext } from "../context/ProfileContext";
 import adapterMovdia from "../adapters/adapter.movdia";
 import { createMovement, getMovementByDate } from "../service/service.movements";
 import { getLocalTimeZone, now } from "@internationalized/date";
 
 
-const movement_type = [{label:"Egreso"}, {label:"Ingreso"}]
+const movement_type = [{ label: "Egreso" }, { label: "Ingreso" }]
 const CardBancos = ({ cuenta, index }) => {
   return (
     <div className="flex w-full border rounded-lg p-4 justify-between">
@@ -43,14 +43,17 @@ const Home = () => {
   const [movement, setMovement] = useState({ institution_id, fecha_movimiento: new Date().toISOString().split('T')[0] })
   const [fechaMovimientos, setFechaMovimientos] = useState(new Date().toISOString().split("T")[0])
   const [searchedMovement, setSearchedMovement] = useState(null)
-  const handleChange = (e) => {
-    console.log("este es el change", e.target.value)
-    const { name, value } = e.target
-    setMovement({
-      ...movement, [name]: value
-    })
-  }
 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+
+
+    const response = await createMovement(data)
+    console.log(e)
+  }
 
   useEffect(() => {
     const today = new Date();
@@ -59,7 +62,7 @@ const Home = () => {
     setFechaMovimientos(formattedDate)
   }, []);
   useEffect(() => {
-    getMovementByDate(fechaMovimientos, setSearchedMovement)
+    getMovementByDate(fechaMovimientos, institution_id, setSearchedMovement)
   }, [fechaMovimientos])
   return (
     <div>
@@ -84,108 +87,110 @@ const Home = () => {
         <div className="flex flex-row gap-4">
           <div className="rounded-lg border flex-grow-[4]">
             <div className="w-60 m-2">
-              <DatePicker onChange={(e)=>{setFechaMovimientos(e)}}></DatePicker>
+              <DatePicker onChange={(e) => { setFechaMovimientos(e) }}></DatePicker>
+              
             </div>
+
             <div>
-              {searchedMovement?.map((mov, index) => (
-                <div>{mov.description}- {mov.amount}</div>
-              ))}
+              <Table shadow="none">
+                <TableHeader>
+                  <TableColumn>Banco</TableColumn>
+                  <TableColumn>Descripción </TableColumn>
+                  <TableColumn>Monto</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  {searchedMovement?.map((mov, index) => {
+                    const bank = banks.find(bank => bank.account_number === mov.bank_id)
+                    console.log(bank)
+                    return (
+                      <TableRow key={index}>
+                        <TableCell>
+                          <div className="flex gap-2 items-center">
+                            <Avatar src={bank.img} alt={bank.bank_name} className="w-8 h-8" />
+                            <div className="flex flex-col">
+                              <span className="text-small">{bank.bank_name}</span>
+                              <span className="text-tiny text-default-400">{bank.account_number}</span>
+                            </div>
+                          </div>
+                        </TableCell>
+                        <TableCell>{mov.description}</TableCell>
+                        <TableCell>
+                          {
+                            mov.amount > 0 ?
+                              <span className="bg-green-500/70 text-white font-semibold rounded-md p-1 px-2">{formatArs.format(mov.amount)}</span>
+                              : <span className="bg-red-500/80 text-white font-semibold rounded-md p-1 px-2">{formatArs.format(mov.amount)}</span>
+                          }
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Table>
             </div>
           </div>
           <div className="rounded-lg border flex-grow-[1]">
             <div className="flex flex-col gap-2 p-2">
               <h1 className="text-2xl font-bold">Crear movimiento</h1>
               <span className="text-sm text-default-500">Crea un nuevo movimiento</span>
-              <Select
-                aria-label='Selecciona un banco'
-                items={banks}
-                name="bank_id"
-                // label='Selecciona un banco' 
-                placeholder='Selecciona un banco'
-                onChange={handleChange}
-                renderValue={(items) => {
-                  return items.map((item) => {
-                    return (
-                      <div className="flex gap-2 items-center">
-                        <Avatar src={item.data.img} alt={item.data.label} className="w-8 h-8" />
-                        <div className='flex flex-col'>
-                          <span className="text-small">{item.data.label}</span>
-                          <span className="text-tiny text-default-400">{item.data.value}</span>
-                        </div>
-                      </div>
-                    )
-                  })
-                }}
-                size='lg'
-              >
-                {
-                  (bank) => {
-                    return (
-                      <SelectItem key={bank.value} textValue={bank.label}>
+              <form onSubmit={(e) => handleSubmit(e)}>
+                <Select
+                  aria-label='Selecciona un banco'
+                  items={banks}
+                  name="bank_id"
+                  // label='Selecciona un banco' 
+                  placeholder='Selecciona un banco'
+                  // onChange={handleChange}
+                  renderValue={(items) => {
+                    return items.map((item) => {
+                      return (
                         <div className="flex gap-2 items-center">
-                          <Avatar src={bank.img} alt={bank.label} className="w-8 h-8" />
+                          <Avatar src={item.data.img} alt={item.data.label} className="w-8 h-8" />
                           <div className='flex flex-col'>
-                            <span className="text-small">{bank.label}</span>
-                            <span className="text-tiny text-default-400">{bank.value}</span>
+                            <span className="text-small">{item.data.label}</span>
+                            <span className="text-tiny text-default-400">{item.data.value}</span>
                           </div>
                         </div>
-                      </SelectItem>
-                    )
+                      )
+                    })
+                  }}
+                  size='lg'
+                >
+                  {
+                    (bank) => {
+                      return (
+                        <SelectItem key={bank.value} textValue={bank.label}>
+                          <div className="flex gap-2 items-center">
+                            <Avatar src={bank.img} alt={bank.label} className="w-8 h-8" />
+                            <div className='flex flex-col'>
+                              <span className="text-small">{bank.label}</span>
+                              <span className="text-tiny text-default-400">{bank.value}</span>
+                            </div>
+                          </div>
+                        </SelectItem>
+                      )
+                    }
                   }
-                }
-              </Select>
-              <Input placeholder="Descripción" name="description" onChange={handleChange} />
-              <Input placeholder="Monto" name="amount" onChange={handleChange} />
-              <Select>
-                {movement_type.map((type) => {
-                  return (
-                    <SelectItem>{type.label}</SelectItem>
-                  )
-                })}
-              </Select>
-              <Button color="primary" onClick={() => { createMovement(movement) }}>Crear</Button>
+                </Select>
+                <Input placeholder="Descripción" name="description" />
+                <Input placeholder="Monto" name="amount" />
+                <Select name="movement_type">
+                  {movement_type.map((type) => {
+                    return (
+                      <SelectItem key={type.label} value={type.label}>{type.label}</SelectItem>
+                    )
+                  })}
+                </Select>
+                <input type="hidden" name="institution_id" value={institution_id} />
+                <input type="hidden" name="fecha_movimiento" value={fechaMovimientos} />
+                <Button color="primary" type="submit">Crear</Button>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
   )
-  {/*
-      <div>
-       {saldos?.map((cuenta) => (
-        <Card
-          key={cuenta.id}
-          className="w-[350px] hover:shadow-lg transition-shadow"
-          shadow="sm"
-        >
-          <CardHeader className="flex gap-4 items-center">
-            <Avatar
-              alt="Logo del banco"
-              name="tota"
-              src={cuenta.bank_number === "001" ? "" : `https://sib1.interbanking.com.ar/skins/bancos/branding/v2/logo${cuenta.bank_number}.png`}
-            />
-            <div className="flex flex-col">
-              <p className="text-lg font-semibold">{cuenta.account_label}</p>
-              <p className="text-sm text-default-500">{cuenta.account_number}</p>
-            </div>
-          </CardHeader>
 
-          <CardBody className="py-5">
-            <div className="space-y-3">
-              <p className="text-sm text-default-500">Saldo Disponible</p>
-              <p className="text-3xl font-bold text-primary">
-                {formatArs.format(cuenta.balances.current_operating_balance)}
-              </p>
-            </div>
-          </CardBody>
-
-          <CardFooter className="flex justify-between items-center">
-
-          </CardFooter>
-        </Card>
-      ))} 
-    </div>*/}
-  // );
 };
 
 export default Home;
--- src/service/service.movements.js
+++ src/service/service.movements.js
@@ -2,17 +2,22 @@ import { data } from "autoprefixer"
 import axios from "axios"
 
 export const createMovement =  async (data) => {
+    const {movement_type, description, amount,bank_id} = data
+    const movement = {  
+        ...data,
+        amount: movement_type === "Ingreso" ? Number(amount) : -Number(amount),
+    }
     try {
-        const response = axios.post(`${import.meta.env.VITE_API_URL}movement`, data)
+        const response = axios.post(`${import.meta.env.VITE_API_URL}movement`, movement)
     }
     catch(err){
         console.error(err)
     }
 }
 
-export const getMovementByDate = async (fecha,setter) => {
+export const getMovementByDate = async (fecha,institution_id, setter) => {
     try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}movement?fecha=${fecha}`)
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}movement?fecha_movimiento=${fecha}&institution_id=${institution_id}`)
         const data = response.data
         setter(data)
         return data
