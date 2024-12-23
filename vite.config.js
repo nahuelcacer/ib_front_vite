156370ae4c2127b391954e35cddd107bb1421995
Nahuel <nahue_cge@hotmail.com>
2024-12-23 15:49:17 -0300

    s

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,14 @@ import react from '@vitejs/plugin-react'
 //   }
 // })
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://interges-back.vercel.app',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
