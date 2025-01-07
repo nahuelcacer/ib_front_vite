d31e3ae179a4db17bb00ddb1baf7d8c6bcfaf343
Nahuel <nahue_cge@hotmail.com>
2025-01-07 14:31:22 -0300

    Update institution display in MainLayout: Added a fallback value "INS" for institution name when user data is not available, enhancing user experience and preventing potential rendering issues.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -20,7 +20,7 @@ const MainLayout = () => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
             </svg>
 
-            <p className='text-small text-default-500' aria-label="Nombre de la institucion">{user?.institution_data.nombre}</p>
+            <p className='text-small text-default-500' aria-label="Nombre de la institucion">{user?.institution_data.nombre || "INS"}</p>
           </CardHeader>
         </Card>
       </div>
