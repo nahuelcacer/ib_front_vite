4313854e21ec9a4f822f9860dab924f07908d05c
Nahuel <nahue_cge@hotmail.com>
2025-01-07 14:50:38 -0300

    Refactor Movimientos component to utilize authService for user data retrieval: Replaced ProfileContext with authService.getUser() for improved clarity and direct access to user information. This change enhances the component's efficiency and aligns with recent authentication updates.

--- src/pages/Movimientos.jsx
+++ src/pages/Movimientos.jsx
@@ -2,14 +2,14 @@ import { Tab, Tabs } from '@nextui-org/react'
 import React, { useContext, useState } from 'react'
 import MovimientoDia from './MovimientoDia'
 import MovimientoAnteriores from './MovimientoAnteriores'
-import { ProfileContext } from '../context/ProfileContext'
 import adapterMovdia from '../adapters/adapter.movdia'
 import { useDispatch } from 'react-redux'
 import { request, reset } from '../features/bank/bank.slices'
 import { reset as resetMovDia } from '../features/movdia/movdia.slices'
 import { reset as resetMovAnterior } from '../features/movanterior/movanterior.slices'
+import authService from '../service/auth'
 const Movimientos = () => {
-  const { accounts, institution_id, id, customer_id, client_id, token_ib } = useContext(ProfileContext)
+  const { accounts, institution_id, id, customer_id, client_id, token_ib } = authService.getUser()
   const [banks, setBanks] = useState(accounts.map(
     account => adapterMovdia(account)
   ))
