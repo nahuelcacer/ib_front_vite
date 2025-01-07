e080002c06bcfe3d3384f4aad846c019ef15435e
Nahuel <nahue_cge@hotmail.com>
2025-01-07 14:08:29 -0300

    Refactor MainLayout to utilize authService for user data retrieval: Removed unused institution state and API call for institutions, replacing it with direct access to user information from authService. Updated institution display to reflect user data, enhancing clarity and reducing unnecessary complexity.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,25 +5,12 @@ import { ProfileContext } from '../context/ProfileContext'
 import { Outlet } from 'react-router-dom'
 import { getInstitutions } from '../service/service.instituions'
 import { Card, CardHeader, Select, SelectItem } from '@nextui-org/react'
+import authService from '../service/auth'
 
 
 const MainLayout = () => {
-  const { institution } = useContext(ProfileContext)
-
-  const [institutions, setInstitutions] = useState(null)
-
-  // const [selectedInstitution, setSelectedInstitution] = useState(null)
-  useEffect(() => {
-    const fetchInstitutions = async () => {
-      const data = await getInstitutions()
-      // setInstitutions(data)
-
-    }
-    fetchInstitutions()
-    //   const selectedInstitution = JSON.parse(sessionStorage.getItem('user')).institution_id
-    //   setSelectedInstitution(institution?.find(inst => inst.id === parseInt(selectedInstitution)))
-  }, [])
- 
+  const user = authService.getUser()
+   console.log(user, "ESTE ES EL YUSER DE MAIN")
   return (
     <div className="flex" aria-label="Layout principal">
       <div className="fixed top-4 right-4 z-50" aria-label="Institucion">
@@ -33,7 +20,7 @@ const MainLayout = () => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
             </svg>
 
-            <p className='text-small text-default-500' aria-label="Nombre de la institucion">{institution?.nombre}</p>
+            <p className='text-small text-default-500' aria-label="Nombre de la institucion">{user?.institution_data.nombre}</p>
           </CardHeader>
         </Card>
       </div>
