87b7f5c7a63d574208c1ac446578ef0b1768d64e
Nahuel <nahue_cge@hotmail.com>
2025-01-27 11:57:48 -0300

    fix the url function registerReceipt

--- src/service/service.movements.js
+++ src/service/service.movements.js
@@ -1,28 +1,27 @@
-import { data } from "autoprefixer"
 import axios from "axios"
 
-export const createMovement =  async (data) => {
-    const {movement_type, description, amount,bank_id} = data
-    const movement = {  
+export const createMovement = async (data) => {
+    const { movement_type, description, amount, bank_id } = data
+    const movement = {
         ...data,
         amount: movement_type === "Ingreso" ? Number(amount) : -Number(amount),
     }
     try {
-        
+
         const response = axios.post(`${import.meta.env.VITE_API_URL}movement`, movement)
     }
-    catch(err){
+    catch (err) {
         console.error(err)
     }
 }
 
-export const getMovementByDate = async (fecha,institution_id, setter) => {
+export const getMovementByDate = async (fecha, institution_id, setter) => {
     try {
         const response = await axios.get(`${import.meta.env.VITE_API_URL}movement?fecha_movimiento=${fecha}&institution_id=${institution_id}`)
         const data = response.data
         setter(data)
         return data
-    }catch(err){
+    } catch (err) {
         setter(null)
         console.error(err)
     }
@@ -32,7 +31,7 @@ export const deleteMovement = async (id) => {
     try {
         const response = await axios.delete(`${import.meta.env.VITE_API_URL}movement/${id}`)
         return response.data
-    }catch(err){
+    } catch (err) {
         console.error(err)
     }
 }       
--- src/service/service.receipt.js
+++ src/service/service.receipt.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const registerReceipt = async (data) => {
   try {
-    const response = axios.post(`${import.meta.env.VITE_API_URL}/receipt`, data)
+    const response = axios.post(`${import.meta.env.VITE_API_URL}receipt`, data)
     return response.json()
   } catch (error) {
     console.error(error)
